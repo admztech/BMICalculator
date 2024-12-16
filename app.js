d910f293a782312ab44e976bf60cb6ead47b44d0
Adnan <adnandesign47@gmail.com>
2024-12-16 15:50:44 +0500

    updates

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ form.addEventListener('submit',function(event){
     const height = parseInt(document.querySelector('#hgt').value);
     const weight = parseInt(document.querySelector('#wgt').value);
     const result = document.querySelector("#result");
-    
+
     if(height === "" || height === 0 || isNaN(height)){
         result.innerHTML= `Please give a valid height : ${height}`;
-    }else if (weight === "" || weight === 0 || isNaN(weight)){
+    }
+    else if (weight === "" || weight === 0 || isNaN(weight)){
         result.innerHTML = `Please give a valid weight : ${weight}`;
-    }else{
+    }
+    else{
         const bmi = ( weight / (( height * height ) / 10000 )).toFixed(2);
         if(bmi > 24.9){
             result.innerHTML = `<span> <strong> ${bmi} </strong>:  Over Weight</span> `;
